b762c5bda3627adbf23c1ab6739ea297c55ea0b1
Makrelli <kevin.mattdoran@gmail.com>
2020-05-16 16:49:16 +0100

    First Commit

---
+++ Audio Editing/GU-on_Package.lua
@@ -0,0 +1,20 @@
+--[[
+ ReaScript Name: Items Editing
+ MetaPackage: true
+ Author: GU-on
+ REAPER: 6+
+ Extensions: SWS
+ Version: 1.0
+ Provides:
+  [Main] GU-on_Item Fader.lua
+ About:
+  Offers relative fade processing
+--[[
+	
+	
+	
+--[[
+ * Changelog:
+   * v1.0 (2020-05-16)
+	+ Initial commit
+]]
