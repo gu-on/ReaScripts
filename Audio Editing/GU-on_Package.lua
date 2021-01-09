f34f311d262d59ad936f968689685803d6229cfc
MrMakrelli <kevin.mattdoran@gmail.com>
2021-01-09 16:32:54 +0000

    Updated Package

--- Audio Editing/GU-on_Package.lua
+++ Audio Editing/GU-on_Package.lua
@@ -9,6 +9,7 @@
   [Main] GU-on_Item Fader.lua
   [Main] GU-on_Swap Marker Names with Marker Notes.lua
   [Main] GU-on_Swap Region Names with Region Notes.lua
+  [Main] GU-on_Unselect Muted Items in Selection.lua
  About:
   A collection of my custom scripts
 --[[
@@ -21,4 +22,5 @@
 	+ Initial commit
    * v1.1 (2021-01-09)
     + added Marker/Region Notes Swapper
+	+ added Unselect Muted
 ]]
